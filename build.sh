1115282b3522c64fdc0ef0f164d86e868ec9c9bc
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2021-08-18 22:23:15 +0100

    Modify bas2tap to include program name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 pasmo --tap main.z80 main.tap
-bas2tap -a loader.bas
+bas2tap -a10 -sloader loader.bas loader.tap
 cat loader.tap main.tap > program.tap
 fuse --tape program.tap -g 4x
